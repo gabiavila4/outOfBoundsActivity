7d17c1bf56fa1943362ca8d6e11b074188c3038c
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-10-18 00:44:36 -0500

    10/18

--- outOfBoundsActivity/main.cpp
+++ outOfBoundsActivity/main.cpp
@@ -6,9 +6,48 @@
 //
 
 #include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class out_of_bounds : public exception{
+public:
+    virtual const char* what() const throw(){
+        return "Index Out of Bounds";
+    }
+} oob;
+
+string findInArray(vector<int> v, int idx){
+    
+    if (idx >= 0 && idx < v.size()){
+        string s = to_string(v[idx]);
+        cout << s << endl;
+        return s;
+    }else{
+        throw oob;
+    }
+}
+
+int main() {
+   
+    vector<int> vec = {31, 19, 23, 42, 53};
+    //string test = findInArray(vec, 6);
+    try{
+        findInArray(vec, 3);
+    }catch(out_of_bounds& oob){
+        cout << oob.what() << endl;
+    }try{
+        findInArray(vec, 4);
+    }catch(out_of_bounds& oob){
+        cout << oob.what() << endl;
+    }try{
+        findInArray(vec, 32);
+    }catch(out_of_bounds& oob){
+        cout << oob.what() << endl;
+    }try{
+        findInArray(vec, -1);
+    }catch(out_of_bounds& oob){
+        cout << oob.what() << endl;
+    }
+  
 }
